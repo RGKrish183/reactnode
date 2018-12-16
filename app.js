8a6727ab72ce03b9f0964e05b1742112de51a4cc
danesh <danesh.g@hoffensoft.com>
2018-12-16 18:35:25 +0530

    react node

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
-var express=require('express');
-var path=require('path');
-var cookieParser =require('cookie-parser');
-var logger =require('morgan');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 var cors=require('cors');
 var bodyParser=require('body-parser');
+const config = require('./config');
+const mongoose=require('mongoose');
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+
+
+mongoose.connect(config.db.url, { useNewUrlParser: true }).then(
+    () => console.log('Database connected successfully'),
+    (err) => { console.log(err); }
+  );
 
 var app = express();
 
---
+++ config.js
@@ -0,0 +1,27 @@
+require('dot-env');
+const env = process.env.NODE_ENV; // 'dev' or 'test'
+
+const dev = {
+ app: {
+   port: 3000
+ },
+ db: {
+   url: 'mongodb://localhost/testdb'
+   }
+};
+
+const prod = {
+ app: {
+   port: 3000
+ },
+ db: {
+    url: 'mongodb://localhost/test'
+ }
+};
+
+const config = {
+ dev,
+ prod
+};
+
+module.exports = config[env];
